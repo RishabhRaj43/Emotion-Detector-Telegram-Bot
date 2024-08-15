195bff352ad50e8b00ad0092b394ee85bc9e58ae
Rishabh Raj Verma <86555688+RishabhRaj43@users.noreply.github.com>
2024-08-16 00:15:12 +0530

    Deleted app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const latitude = 22.60027;
-const longitude = 88.449982;
-const api_id = `https://api.tomorrow.io/v4/weather/forecast?location=${latitude},${longitude}&apikey=3ZBGUKUN2Nlnp5tbt6bQU9y5UJNN3LXO`;
-
-const res = await axios.get(api_id);
-
-console.log(res.data.timelines.daily[0].values.temperatureAvg);
